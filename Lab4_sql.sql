2bf5250f8aec6c1a50ff540c52ec80a12f56385b
susobhan-s <suso.sam@gmail.com>
2023-04-22 11:43:24 +0530

    add new sql file

--- Lab4_sql.sql
+++ Lab4_sql.sql
@@ -74,3 +74,171 @@ CREATE TABLE IF NOT EXISTS rating (
     PRIMARY KEY (RAT_ID),
     FOREIGN KEY (ORD_ID) REFERENCES `order`(ORD_ID)
 );
+
+------------------ Insert Data into supplier table ----------------------
+
+INSERT INTO SUPPLIER VALUES(1,"Rajesh Retails","Delhi",'1234567890');
+INSERT INTO SUPPLIER VALUES(2,"Appario Ltd.","Mumbai",'2589631470');
+INSERT INTO SUPPLIER VALUES(3,"Knome products","Banglore",'9785462315');
+INSERT INTO SUPPLIER VALUES(4,"Bansal Retails","Kochi",'8975463285');
+INSERT INTO SUPPLIER VALUES(5,"Mittal Ltd.","Lucknow",'7898456532');
+
+----------------- Insert data into Customer table --------------------------
+
+INSERT INTO CUSTOMER VALUES(1,"AAKASH",'9999999999',"DELHI",'M');
+INSERT INTO CUSTOMER VALUES(2,"AMAN",'9785463215',"NOIDA",'M');
+INSERT INTO CUSTOMER VALUES(3,"NEHA",'9999999999',"MUMBAI",'F');
+INSERT INTO CUSTOMER VALUES(4,"MEGHA",'9994562399',"KOLKATA",'F');
+INSERT INTO CUSTOMER VALUES(5,"PULKIT",'7895999999',"LUCKNOW",'M');
+
+-------------------- Insert data into Category table --------------------------
+
+INSERT INTO CATEGORY VALUES( 1,"BOOKS");
+INSERT INTO CATEGORY VALUES(2,"GAMES");
+INSERT INTO CATEGORY VALUES(3,"GROCERIES");
+INSERT INTO CATEGORY VALUES (4,"ELECTRONICS");
+INSERT INTO CATEGORY VALUES(5,"CLOTHES");
+
+--------------------- Insert data into Product table -------------------------------
+
+INSERT INTO PRODUCT VALUES(1,"GTA V","Windows 7 and above with i5 processor and 8GB RAM",2);
+INSERT INTO PRODUCT VALUES(2,"TSHIRT","SIZE-L with Black, Blue and White variations",5);
+INSERT INTO PRODUCT VALUES(3,"ROG LAPTOP","Windows 10 with 15inch screen, i7 processor, 1TB SSD",4);
+INSERT INTO PRODUCT VALUES(4,"OATS","Highly Nutritious from Nestle",3);
+INSERT INTO PRODUCT VALUES(5,"HARRY POTTER","Best Collection of all time by J.K Rowling",1);
+INSERT INTO PRODUCT VALUES(6,"MILK","1L Toned MIlk",3);
+INSERT INTO PRODUCT VALUES(7,"Boat EarPhones","1.5Meter long Dolby Atmos",4);
+INSERT INTO PRODUCT VALUES(8,"Jeans","Stretchable Denim Jeans with various sizes and color",5);
+INSERT INTO PRODUCT VALUES(9,"Project IGI","compatible with windows 7 and above",2);
+INSERT INTO PRODUCT VALUES(10,"Hoodie","Black GUCCI for 13 yrs and above",5);
+INSERT INTO PRODUCT VALUES(11,"Rich Dad Poor Dad","Written by RObert Kiyosaki",1);
+INSERT INTO PRODUCT VALUES(12,"Train Your Brain","By Shireen Stephen",1);
+
+------------------------------- Insert data into Supplier Pricing table ------------------
+
+INSERT INTO SUPPLIER_PRICING VALUES(1,1,2,1500);
+INSERT INTO SUPPLIER_PRICING VALUES(2,3,5,30000);
+INSERT INTO SUPPLIER_PRICING VALUES(3,5,1,3000);
+INSERT INTO SUPPLIER_PRICING VALUES(4,2,3,2500);
+INSERT INTO SUPPLIER_PRICING VALUES(5,4,1,1000);
+INSERT INTO SUPPLIER_PRICING VALUES(6,12,2,780);
+INSERT INTO SUPPLIER_PRICING VALUES(7,12,4,789);
+INSERT INTO SUPPLIER_PRICING VALUES(8,3,1,31000);
+INSERT INTO SUPPLIER_PRICING VALUES(9,1,5,1450);
+INSERT INTO SUPPLIER_PRICING VALUES(10,4,2,999);
+INSERT INTO SUPPLIER_PRICING VALUES(11,7,3,549);
+INSERT INTO SUPPLIER_PRICING VALUES(12,7,4,529);
+INSERT INTO SUPPLIER_PRICING VALUES(13,6,2,105);
+INSERT INTO SUPPLIER_PRICING VALUES(14,6,1,99);
+INSERT INTO SUPPLIER_PRICING VALUES(15,2,5,2999);
+INSERT INTO SUPPLIER_PRICING VALUES(16,5,2,2999);
+
+------------------------------ Insert data into Order table -----------------------------
+
+INSERT INTO `ORDER` VALUES (101,1500,"2021-10-06",2,1);
+INSERT INTO `ORDER` VALUES(102,1000,"2021-10-12",3,5);
+INSERT INTO `ORDER` VALUES(103,30000,"2021-09-16",5,2);
+INSERT INTO `ORDER` VALUES(104,1500,"2021-10-05",1,1);
+INSERT INTO `ORDER` VALUES(105,3000,"2021-08-16",4,3);
+INSERT INTO `ORDER` VALUES(106,1450,"2021-08-18",1,9);
+INSERT INTO `ORDER` VALUES(107,789,"2021-09-01",3,7);
+INSERT INTO `ORDER` VALUES(108,780,"2021-09-07",5,6);
+INSERT INTO `ORDER` VALUES(109,3000,"2021-09-10",5,3);
+INSERT INTO `ORDER` VALUES(110,2500,"2021-09-10",2,4);
+INSERT INTO `ORDER` VALUES(111,1000,"2021-09-15",4,5);
+INSERT INTO `ORDER` VALUES(112,789,"2021-09-16",4,7);
+INSERT INTO `ORDER` VALUES(113,31000,"2021-09-16",1,8);
+INSERT INTO `ORDER` VALUES(114,1000,"2021-09-16",3,5);
+INSERT INTO `ORDER` VALUES(115,3000,"2021-09-16",5,3);
+INSERT INTO `ORDER` VALUES(116,99,"2021-09-17",2,14);
+
+------------------------------ Insert data into Ratings table ----------------------
+
+INSERT INTO RATING VALUES(1,101,4);
+INSERT INTO RATING VALUES(2,102,3);
+INSERT INTO RATING VALUES(3,103,1);
+INSERT INTO RATING VALUES(4,104,2);
+INSERT INTO RATING VALUES(5,105,4);
+INSERT INTO RATING VALUES(6,106,3);
+INSERT INTO RATING VALUES(7,107,4);
+INSERT INTO RATING VALUES(8,108,4);
+INSERT INTO RATING VALUES(9,109,3);
+INSERT INTO RATING VALUES(10,110,5);
+INSERT INTO RATING VALUES(11,111,3);
+INSERT INTO RATING VALUES(12,112,4);
+INSERT INTO RATING VALUES(13,113,2);
+INSERT INTO RATING VALUES(14,114,1);
+INSERT INTO RATING VALUES(15,115,1);
+INSERT INTO RATING VALUES(16,116,0);
+
+-------------------- Query Question 3 -----------------------------
+
+select cus_gender as Gender, count(cus_gender) as Count from customer
+where cus_id in (select cus_id from `order` group by cus_id having sum(ord_amount)>=3000)
+group by cus_gender;
+
+---------------------------- Query Question 4 -----------------------------------------
+
+select o.*, p.pro_name as PRODUCT_NAME from `order` o, supplier_pricing sp, product p where
+o.cus_id = 2 and
+o.pricing_id = sp.pricing_id and
+sp.pro_id = p.pro_id;
+
+-------------------------- Query Question 5 ------------------------------------------
+
+select * from supplier where
+supp_id in (select supp_id from supplier_pricing group by supp_id having count(supp_id) > 1)
+;
+
+------------------------- Query Question 6 -----------------------------------------
+
+select prd.cat_id, cat_name , pro_name , min(supp_price) Price
+from supplier_pricing sp
+join product prd
+join category cat
+on sp.pro_id = prd.pro_id and 
+prd.cat_id = cat.cat_id
+group by prd.cat_id
+;
+
+----------------------- Query Question 7 ---------------------------------------
+
+select p.pro_id PRODUCT_ID, p.pro_name as PRODUCT_NAME 
+from `order` o, supplier_pricing sp, product p 
+where
+o.pricing_id = sp.pricing_id and
+sp.pro_id = p.pro_id and
+o.ord_date > '2021-10-05';
+
+----------------------- Query Question 8 ----------------------
+
+select cus_name, cus_gender 
+from customer
+where lcase(cus_name) like '%a'
+or lcase(cus_name) like 'a%'
+;
+
+------------------------- Stored Proc Question 9 -------------------------
+
+delimiter $$
+create procedure avg_rating_proc()
+begin
+select t.*,
+case when avg_rating = 5 then 'Excellent Service'
+when avg_rating > 4 then 'Good Service'
+when avg_rating > 2 then 'Average Service'
+else 'Poor Service'
+end TYPE_OF_SERVICE
+from (
+select s.supp_id, s.supp_name, avg(r.rat_ratstars) avg_rating
+from supplier s, supplier_pricing sp, `order` o, rating r
+where
+s.supp_id = sp.supp_id and
+sp.pricing_id = o.pricing_id and
+o.ord_id = r.ord_id
+group by s.supp_id
+) T ;
+end $$
+delimiter ;
+
+call avg_rating_proc;
